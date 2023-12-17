3b1759e093ae7d628f605550fcf486a770770795
Michał Nadolny <michalnadolny192@gmai.com>
2023-12-17 15:37:56 +0100

    added user with admin role

--- src/main/java/mn/michal/onlineshopapp/OnlineShopAppApplication.java
+++ src/main/java/mn/michal/onlineshopapp/OnlineShopAppApplication.java
@@ -11,9 +11,6 @@ public class OnlineShopAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineShopAppApplication.class, args);
-		String adminPass = "admin";
-		var encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		System.out.println(encoder.encode(adminPass));
 	}
 
 }
